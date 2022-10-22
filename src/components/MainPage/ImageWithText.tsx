225059039a46585365440c01c3f7ab49f457154b
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-10-22 17:31:09 +0300

    reduce description length

--- src/components/MainPage/ImageWithText.tsx
+++ src/components/MainPage/ImageWithText.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import Square from '~/svg/Square.svg';
 // interface ImageWithTextProps {
 
 // }
@@ -8,9 +6,9 @@ const ImageWithText: React.FunctionComponent = () => {
   return (
     <div className=''>
       <div className=''>
-        <div className=' relative  overflow-scroll text-white    '>
-          <div className=' absolute   object-cover p-6 pt-24 text-left  text-white  sm:bottom-20 lg:border-black '>
-            <div className='bg-slate-700  p-6 md:bg-none lg:w-2/3'>
+        <div className=' relative  text-white    '>
+          <div className=' absolute   overflow-scroll object-cover p-6 pt-24 text-left  text-white  sm:bottom-20 lg:border-black '>
+            <div className='bg-slate-700  p-6 md:bg-none lg:w-2/3 '>
               <h3>Название Компании</h3>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Doloribus, praesentium reprehenderit perspiciatis laborum enim ea
@@ -23,18 +21,12 @@ const ImageWithText: React.FunctionComponent = () => {
               adipisicing elit. Blanditiis iusto repudiandae quia molestiae nemo
               cum minus accusamus omnis laudantium ea optio quidem cumque,
               suscipit tempora eos provident error ad quos! Lorem ipsum dolor
-              sit amet consectetur adipisicing elit. Minus accusamus esse odit
-              sed officiis reprehenderit sapiente vero praesentium suscipit non
-              sint, repudiandae quidem obcaecati maiores in, eligendi aut beatae
-              numquam! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Sit, magni odio maxime qui labore ea veritatis ratione, delectus
-              vero, error consequuntur animi eos cum? Blanditiis corporis omnis
-              non ad. Obcaecati?
+              sit amet consectetur adipisicing elit.
             </div>
           </div>
           <img
             src='https://thumbs.dreamstime.com/z/gas-turbine-engine-feed-gas-compressor-located-inside-pressurized-enclosure-gas-turbine-engine-used-offshore-gas-turbine-117572042.jpg'
-            className=' min-h-[700px] w-full object-none mix-blend-overlay blur-sm lg:max-h-[800px] xl:object-cover '
+            className=' min-h-[750px] w-full object-none mix-blend-overlay blur-sm lg:max-h-[800px] xl:object-cover '
           />
         </div>
       </div>
---
+++ src/components/Partnership/Partners.tsx
@@ -0,0 +1,9 @@
+interface PartnersProps {
+  description: string;
+}
+
+const Partners: React.FunctionComponent<PartnersProps> = () => {
+  return <div></div>;
+};
+
+export default Partners;
