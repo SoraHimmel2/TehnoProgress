55e4b1b43db91517c3e8a93bec968531e20e74c6
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-10-27 14:57:16 +0300

    remove contacts content

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -3,10 +3,7 @@ import ContactsInfo from '@/components/Contacts/ContactsInfo';
 
 const Contacts: React.FunctionComponent = () => {
   return (
-    <div className='mt-24 flex flex-col items-center gap-10 sm:flex-row sm:justify-center'>
-      <ContactsInfo></ContactsInfo>
-      <Requisites></Requisites>
-    </div>
+    <div className='mt-24 flex flex-col items-center gap-10 sm:flex-row sm:justify-center'></div>
   );
 };
 
