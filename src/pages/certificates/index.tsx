75dc8a03578ca6a11fad1447e17fc6b13abd1ff2
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-10-27 10:56:04 +0300

    change element place

--- src/pages/certificates/index.tsx
+++ src/pages/certificates/index.tsx
@@ -6,8 +6,8 @@ const Certificate: React.FunctionComponent = () => {
   const imageSrc = query.certificateName as string;
 
   return (
-    <div className='flex flex-col justify-center'>
-      <img className='mt-24' src={`/images/Festo.jpg`}></img>
+    <div className='mt-24 flex flex-col justify-center gap-8 md:flex-row'>
+      <img className='mb-4' src={`/images/Festo.jpg`}></img>
       <img className='m-4' src={`/images/Aignep.png`}></img>
     </div>
   );
