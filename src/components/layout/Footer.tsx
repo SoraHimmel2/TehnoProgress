8ee6e966c4493d749d5a5735de131c78e41e5c7b
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-10-28 11:14:24 +0300

    change font

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -33,22 +33,22 @@ const Footer: React.FunctionComponent = () => {
           <div className='flex flex-col items-start  border-black'>
             <img src='/images/logo-vq.png' className='w-72 md:w-60'></img>
             <h6 className=' mb-4 font-semibold uppercase'>Контакты</h6>
-            <div className='w-60 flex-col flex-wrap font-sans'>
+            <div className='w-60 flex-col flex-wrap '>
               <div className=''>Адрес:&nbsp;</div>
               <div className=''>г. Санкт-Петербург,</div>
               <div>ул. 6-я Красноармейская,</div>
               <div className='mb-2 '>д. 10, лит. А, ПОМЕЩ. 24</div>
             </div>
 
-            <div className='mb-2 flex-col flex-wrap font-sans'>
+            <div className='mb-2 flex-col flex-wrap '>
               <div className=''>Почта: &nbsp;</div>
               <div className=''>sales@tehnoprogress78.ru</div>
             </div>
-            <div className='mb-2 flex-col flex-wrap font-sans '>
+            <div className='mb-2 flex-col flex-wrap  '>
               <div className=''>Отдел продаж: </div>
               <div className=''> 8-921-956-09-42</div>
             </div>
-            <div className='mb-2 flex-col flex-wrap font-sans '>
+            <div className='mb-2 flex-col flex-wrap  '>
               <div className=''>Директор: &nbsp; &nbsp; </div>
               <div className=' '> 8-921-211-09-40</div>
             </div>
@@ -60,38 +60,38 @@ const Footer: React.FunctionComponent = () => {
             </h6>
             <div className='flex flex-col items-start gap-8 lg:flex-row'>
               <div>
-                <div className='flex-col  flex-wrap font-sans'>
+                <div className='flex-col  flex-wrap '>
                   <div className=''>Банк: &nbsp;</div>
                   <div className='mb-2'>Филиал &nbsp;</div>
                   <div className='mb-2 '>"Северная столица"</div>
                   <div className='mb-2 '>АО "Райфайзенбанк"</div>
                 </div>
-                <div className=' flex-col flex-wrap font-sans '>
+                <div className=' flex-col flex-wrap  '>
                   <div className=''>БИК банка:&nbsp;</div>
                   <div className='mb-2 '>044030723</div>
                 </div>
-                <div className='mb-2 flex-col flex-wrap font-sans'>
+                <div className='mb-2 flex-col flex-wrap '>
                   <div className=''>ИНН: &nbsp;</div>
                   <div className=''>7801688448</div>
                 </div>
-                <div className='mb-2 flex-col flex-wrap font-sans '>
+                <div className='mb-2 flex-col flex-wrap  '>
                   <div className=''>ОГРН: &nbsp;</div>
                   <div className=' '>1207800126247</div>
                 </div>
               </div>
 
               <div>
-                <div className='mb-2 w-80 flex-col flex-wrap font-sans'>
+                <div className='mb-2 w-80 flex-col flex-wrap '>
                   <div className=''>
                     Расчетный счет: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                   </div>
                   <div className=' '>40702810403000066173</div>
                 </div>
-                <div className='mb-2 flex w-80 flex-col flex-wrap font-sans'>
+                <div className='mb-2 flex w-80 flex-col flex-wrap '>
                   <div className=''>Корреспондентский счет: &nbsp;</div>
                   <div className=' '>30101810100000000723</div>
                 </div>
-                <div className='mb-2 flex-col flex-wrap font-sans '>
+                <div className='mb-2 flex-col flex-wrap  '>
                   <div className=''>КПП: &nbsp;</div>
                   <div className=''>&nbsp;783801001</div>
                 </div>
