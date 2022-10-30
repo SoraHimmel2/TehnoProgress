cef46d40b9d67f4de5bfc5284578c8d0d0a6ec1d
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-10-30 09:00:46 +0300

    change main page

--- src/components/MainPage/ImageWithText.tsx
+++ src/components/MainPage/ImageWithText.tsx
@@ -4,26 +4,9 @@
 
 const ImageWithText: React.FunctionComponent = () => {
   return (
-    <div className=''>
-      <div className=' relative  text-white    '>
-        <div className='  bottom-14 m-14 object-cover p-6 pt-24  text-left  text-black lg:border-black '>
-          Приятно познакомиться! Команда ООО "ТЕХНОПРОГРЕСС" состоит из опытных
-          специалистов в области автоматики. Мы считаем автоматизацию делом
-          своей жизни и связываем свою жизнь с этой профессией много лет. Весь
-          наш опыт и знания к вашим услугам! Сегодня мы можем предложить вам:
-          профессиональную команду инженеров, готовую к решению Ваших задач в
-          области пневмоавтоматики; Пневматику любого вида и сложности(подбор
-          аналогов); Электропривод (серво- и шаговый), контроллеры; Промышленную
-          запорную/регулирующую арматуру всех типов. Техническую поддержку по
-          подбору оборудования; Проектирование и изготовление шкафов управления;
-          Манипуляторы на базе сервопривода для перемещения объектов; … и многое
-          другое! Пишите на электронную почту или просто звоните!
-        </div>
-        {/* <img
-          src='https://thumbs.dreamstime.com/z/gas-turbine-engine-feed-gas-compressor-located-inside-pressurized-enclosure-gas-turbine-engine-used-offshore-gas-turbine-117572042.jpg'
-          className='min-h-[750px] w-full object-none mix-blend-overlay blur-sm brightness-125 lg:max-h-[800px] xl:object-cover '
-        /> */}
-      </div>
+    <div className='p-10 text-left'>
+      ООО "ТЕХНОПРОГРЕСС" - поставка комплектующих для производства: Пневматика,
+      арматура, подшипники, конвейерные комплектующие!
     </div>
   );
 };
@@ -58,3 +41,12 @@ export default ImageWithText;
 //     </div>
 //   );
 // };
+{
+  /* <div className=' text-center    '>
+<div className='  bottom-14 m-14 object-cover p-6 pt-24  text-left  text-black lg:border-black '></div>
+
+{/* <img
+  src='https://thumbs.dreamstime.com/z/gas-turbine-engine-feed-gas-compressor-located-inside-pressurized-enclosure-gas-turbine-engine-used-offshore-gas-turbine-117572042.jpg'
+  className='min-h-[750px] w-full object-none mix-blend-overlay blur-sm brightness-125 lg:max-h-[800px] xl:object-cover '
+/> */
+}
---
+++ src/components/mainPage/Collaboration.tsx
@@ -0,0 +1,5 @@
+const Collaboration: React.FunctionComponent = () => {
+  return <div></div>;
+};
+
+export default Collaboration;
