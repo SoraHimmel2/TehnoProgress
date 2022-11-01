6e9ab29d92feca591c76bde120c8577489b5719c
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-01 20:10:08 +0300

    change style

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -11,12 +11,12 @@ const SubmitButton: React.FunctionComponent<SubmitButtonProps> = ({
 }) => {
   return (
     <div>
-      <div className='flex h-12 w-full flex-col items-center justify-center bg-sky-400'>
+      <div className='flex h-12 w-40 flex-col items-center justify-center bg-sky-400'>
         <button
-          className='relative h-20 w-60  text-center transition-all
-            duration-500 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-sky-900
+          className='relative h-20 w-40  text-center transition-all
+            duration-500 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-sky-700
             before:opacity-10 before:transition-all before:duration-300 after:absolute
-            after:top-0 after:left-0 after:h-full after:w-full after:scale-125 after:border after:border-sky-900 after:opacity-0
+            after:top-0 after:left-0 after:h-full after:w-full after:scale-125 after:border after:border-sky-700 after:opacity-0
             after:transition-all after:duration-300 before:hover:scale-50 before:hover:opacity-0 after:hover:scale-100
             after:hover:opacity-100'
           type={type}
