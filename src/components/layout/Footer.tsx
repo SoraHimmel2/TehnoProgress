670689d4db64e05658c376ce6f80fb9db1b7891e
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-03 02:48:03 +0300

    Footer: add certificate icon and change layout

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,9 +6,8 @@ const Footer: React.FunctionComponent = () => {
       </div>
       <div className='mx-6  py-10 text-center md:text-left'>
         <div className='grid grid-cols-1 xl:grid-cols-2'>
-          <div className='grid grid-cols-1 items-center  md:grid-cols-2'>
+          <div className='mb-4 grid grid-cols-1  items-center md:grid-cols-2'>
             <div className=''>
-              <h6 className='mb-4 flex  font-semibold uppercase '>&nbsp;</h6>
               <div className='mb-4 flex items-center justify-start'>
                 <svg
                   aria-hidden='true'
@@ -103,41 +102,124 @@ const Footer: React.FunctionComponent = () => {
             </div>
           </div>
 
-          <div className='grid grid-cols-1 items-center   md:grid-cols-2 xl:grid-cols-3'>
-            <div className='mb-4'>
+          <div className='mb-4 grid grid-cols-1   items-center md:grid-cols-2 xl:grid-cols-3'>
+            <div className='mb-4 flex flex-row'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                enable-background='new 0 0 24 24'
+                height='36px'
+                viewBox='0 0 24 24'
+                width='36px'
+                fill='#5f6368'
+              >
+                <g>
+                  <rect fill='none' height='24' width='24' />
+                  <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
+                </g>
+              </svg>
               <div className='text-left'>
                 <div className=''>Банк: &nbsp;</div>
                 <div className=''>Филиал "Северная столица" &nbsp;</div>
                 <div className=''>АО "Райфайзенбанк"</div>
               </div>
             </div>
-            <div className='mb-4'>
+            <div className='mb-4 flex flex-row'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                enable-background='new 0 0 24 24'
+                height='36px'
+                viewBox='0 0 24 24'
+                width='36px'
+                fill='#5f6368'
+                className='invisible'
+              >
+                <g>
+                  <rect fill='none' height='24' width='24' />
+                  <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
+                </g>
+              </svg>
               <div className='text-left'>
                 <div className=''>БИК банка:&nbsp;</div>
                 <div className=' '>044030723</div>
               </div>
             </div>
 
-            <div className='mb-4'>
+            <div className='mb-4 flex flex-row'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                enable-background='new 0 0 24 24'
+                height='36px'
+                viewBox='0 0 24 24'
+                width='36px'
+                fill='#5f6368'
+                className='invisible'
+              >
+                <g>
+                  <rect fill='none' height='24' width='24' />
+                  <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
+                </g>
+              </svg>
               <div className='text-left '>
                 <div className=''>ОГРН: &nbsp;</div>
                 <div className=' '>1207800126247</div>
               </div>
             </div>
-            <div className='mb-4'>
+            <div className='mb-4 flex flex-row'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                enable-background='new 0 0 24 24'
+                height='36px'
+                viewBox='0 0 24 24'
+                width='36px'
+                fill='#5f6368'
+                className='invisible'
+              >
+                <g>
+                  <rect fill='none' height='24' width='24' />
+                  <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
+                </g>
+              </svg>
               <div className='text-left'>
                 <div className=''>Расчетный счет: &nbsp;</div>
                 <div className=' '>40702810403000066173</div>
               </div>
             </div>
 
-            <div className='mb-4'>
+            <div className='mb-4 flex flex-row'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                enable-background='new 0 0 24 24'
+                height='36px'
+                viewBox='0 0 24 24'
+                width='36px'
+                fill='#5f6368'
+                className='invisible'
+              >
+                <g>
+                  <rect fill='none' height='24' width='24' />
+                  <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
+                </g>
+              </svg>
               <div className='text-left'>
                 <div className=''>Корреспондентский счет: &nbsp;</div>
                 <div className=' '>30101810100000000723</div>
               </div>
             </div>
-            <div className='mb-4'>
+            <div className='mb-4 flex flex-row'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                enable-background='new 0 0 24 24'
+                height='36px'
+                viewBox='0 0 24 24'
+                width='36px'
+                fill='#5f6368'
+                className='invisible'
+              >
+                <g>
+                  <rect fill='none' height='24' width='24' />
+                  <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
+                </g>
+              </svg>
               <div className='text-left'>
                 <div className=''>ИНН/КПП: &nbsp;</div>
                 <div className=''>7801688448/783801001</div>
