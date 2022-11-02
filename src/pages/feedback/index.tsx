3aaa760f9fa9a0fdbde36e6493564146f92c5133
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-03 01:48:06 +0300

    change title

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -3,7 +3,7 @@ import SubmitForm from '@/components/Forms/SubmitForm';
 const FeedBack: React.FunctionComponent = () => {
   return (
     <div className='mt-24'>
-      <h1 className='py-4 text-center'>Связаться с нами</h1>
+      <h1 className='py-4 text-center'>Обратная связь</h1>
       <div className='flex  justify-center '>
         <SubmitForm></SubmitForm>
       </div>
