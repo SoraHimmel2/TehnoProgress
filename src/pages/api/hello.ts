1809f34babf28e3a5505e95a9fc02ddc95c87a3e
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-03 03:08:59 +0300

    some changes`

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -8,26 +8,7 @@ import { Telegraf } from 'telegraf';
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
 
   const data = req.body;
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.yandex.ru',
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: 'AmelkinAndreiU@yandex.ru', // generated ethereal user
-      pass: 'skllosfxycctquqy'  // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
-  const mailOptions = {
-    from: 'AmelkinAndreiU@yandex.ru', // sender address
-    to: 'AmelkinAndreiU@yandex.ru', // list of receivers
-    subject: 'Клиенты', // Subject line
-    text: `Телефон: ${data.phone} \nФИО: ${data.name}\nКомпания/должность:\n${data.company}\nEmail: ${data.email}\nКомментарий: ${data.comment}\n`, // plain text body
 
-  };
-  transporter.sendMail(mailOptions);
 
   const BotToken = '5472718351:AAG6QczMElHb4Bxv1dOJWyaLqmFC65SetfM';
   const ChatId = '447411888';
@@ -38,6 +19,29 @@ export default async function hello(req: NextApiRequest, res: NextApiResponse) {
 
   const bot = new Telegraf(BotToken);
   bot.telegram.sendMessage(ChatId, `Телефон: ${data.phone} \nФИО: ${data.name}\nКомпания/должность:\n${data.company}\nEmail: ${data.email}\nКомментарий: ${data.comment}\n`).then(() => {
+
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.yandex.ru',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'AmelkinAndreiU@yandex.ru', // generated ethereal user
+        pass: 'skllosfxycctquqy'  // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+    const mailOptions = {
+      from: 'AmelkinAndreiU@yandex.ru', // sender address
+      to: 'AmelkinAndreiU@yandex.ru', // list of receivers
+      subject: 'Клиенты', // Subject line
+      text: `Телефон: ${data.phone} \nФИО: ${data.name}\nКомпания/должность:\n${data.company}\nEmail: ${data.email}\nКомментарий: ${data.comment}\n`, // plain text body
+
+    };
+    transporter.sendMail(mailOptions);
+
     res.status(200).json({ error: 'ok' });
   })
 
