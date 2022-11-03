f9cdbcf8c081ad245ca7707e3a7ad83812632c69
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-03 03:02:38 +0300

    some changes

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -6,48 +6,44 @@ import { Telegraf } from 'telegraf';
 
 
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
-  
+
   const data = req.body;
   const transporter = nodemailer.createTransport({
     host: 'smtp.yandex.ru',
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-        user: 'AmelkinAndreiU@yandex.ru', // generated ethereal user
-        pass: 'skllosfxycctquqy'  // generated ethereal password
+      user: 'AmelkinAndreiU@yandex.ru', // generated ethereal user
+      pass: 'skllosfxycctquqy'  // generated ethereal password
     },
     tls: {
-        rejectUnauthorized: false
+      rejectUnauthorized: false
     }
-});
-const mailOptions = {
-      from: 'AmelkinAndreiU@yandex.ru', // sender address
-      to: 'AmelkinAndreiU@yandex.ru', // list of receivers
-      subject: 'Клиенты', // Subject line
-      text: `Телефон: ${data.phone} \nФИО: ${data.name}\nКомпания/должность:\n${data.company}\nEmail: ${data.email}\nКомментарий: ${data.comment}\n`, // plain text body
+  });
+  const mailOptions = {
+    from: 'AmelkinAndreiU@yandex.ru', // sender address
+    to: 'AmelkinAndreiU@yandex.ru', // list of receivers
+    subject: 'Клиенты', // Subject line
+    text: `Телефон: ${data.phone} \nФИО: ${data.name}\nКомпания/должность:\n${data.company}\nEmail: ${data.email}\nКомментарий: ${data.comment}\n`, // plain text body
 
-   };
-   transporter.sendMail(mailOptions, () => {
-   
-   
-    res.status(200).send('Email has been sent');
-});
+  };
+  transporter.sendMail(mailOptions);
 
   const BotToken = '5472718351:AAG6QczMElHb4Bxv1dOJWyaLqmFC65SetfM';
   const ChatId = '447411888';
-  
 
 
-   
 
-    const bot = new Telegraf(BotToken);
-    bot.telegram.sendMessage(ChatId, `Телефон: ${data.phone} \nФИО: ${data.name}\nКомпания/должность:\n${data.company}\nEmail: ${data.email}\nКомментарий: ${data.comment}\n`).then(()=>{
-      res.status(200).json({ error: 'ok' });
-    })
 
-    
-    //res.status(400).json({ name: 'not ok' })
-  
-   
+
+  const bot = new Telegraf(BotToken);
+  bot.telegram.sendMessage(ChatId, `Телефон: ${data.phone} \nФИО: ${data.name}\nКомпания/должность:\n${data.company}\nEmail: ${data.email}\nКомментарий: ${data.comment}\n`).then(() => {
+    res.status(200).json({ error: 'ok' });
+  })
+
+
+  //res.status(400).json({ name: 'not ok' })
+
+
 
 }
