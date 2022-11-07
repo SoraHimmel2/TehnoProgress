1ca71d77bd2198ccde8fa6a459dbc5d996373c70
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-07 08:14:30 +0300

    Partnership:change layout

--- src/components/Partnership/PartnerList.tsx
+++ src/components/Partnership/PartnerList.tsx
@@ -11,9 +11,13 @@ const PartnerList: React.FunctionComponent = () => {
     { imageSrc: 'images/products/skf.jpg' },
     { imageSrc: 'images/products/rexnord.jpg' },
     { imageSrc: 'images/products/duplomatic.jpg' },
+    { imageSrc: 'images/products/ntn.jpg' },
+    { imageSrc: 'images/products/optibelt.jpg' },
+    { imageSrc: 'images/products/fag.jpg' },
+    { imageSrc: 'images/products/nsk.jpg' },
   ];
   return (
-    <div className='container grid grid-cols-1 items-center justify-items-center md:grid-cols-2 lg:grid-cols-3'>
+    <div className='container grid grid-cols-1 items-center justify-items-center md:grid-cols-2 lg:grid-cols-5'>
       {partnerList.map((product) => {
         return (
           <Partner
