0a799c4d177a7303d502ded36f440871e50d69ab
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-09 11:42:09 +0300

    Contacts : change layout

--- src/components/Contacts/Reqisites.tsx
+++ src/components/Contacts/Reqisites.tsx
@@ -1,9 +1,12 @@
+import ArrowLink from '@/components/links/ArrowLink';
+
 const Requisites: React.FunctionComponent = () => {
   return (
-    <div className='flex flex-col rounded  border border-primary-600 p-4 text-xl shadow-xl'>
-      <h6 className=' mb-4 font-semibold uppercase'>Реквизиты</h6>
-      <div className='mb-4 flex flex-row'>
-        {/* <svg
+    <div className='flex flex-col items-start  rounded border border-primary-600 p-4 text-xl shadow-xl'>
+      <div>
+        <h6 className=' mb-4 font-semibold uppercase'>Реквизиты</h6>
+        <div className='mb-4 flex flex-row'>
+          {/* <svg
           xmlns='http://www.w3.org/2000/svg'
           enableBackground='new 0 0 34 34'
           height='36px'
@@ -17,14 +20,14 @@ const Requisites: React.FunctionComponent = () => {
             <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
           </g>
         </svg> */}
-        <div className='text-left'>
-          <div className=''>Банк: &nbsp;</div>
-          <div className=''>Филиал "Северная столица" &nbsp;</div>
-          <div className=''>АО "Райфайзенбанк"</div>
+          <div className='text-left'>
+            <div className=''>Банк: &nbsp;</div>
+            <div className=''>Филиал "Северная столица" &nbsp;</div>
+            <div className=''>АО "Райфайзенбанк"</div>
+          </div>
         </div>
-      </div>
-      <div className='mb-4 flex flex-row'>
-        {/* <svg
+        <div className='mb-4 flex flex-row'>
+          {/* <svg
           xmlns='http://www.w3.org/2000/svg'
           enableBackground='new 0 0 24 24'
           height='36px'
@@ -38,14 +41,14 @@ const Requisites: React.FunctionComponent = () => {
             <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
           </g>
         </svg> */}
-        <div className='text-left'>
-          <div className=''>БИК банка:&nbsp;</div>
-          <div className=' '>044030723</div>
+          <div className='text-left'>
+            <div className=''>БИК банка:&nbsp;</div>
+            <div className=' '>044030723</div>
+          </div>
         </div>
-      </div>
 
-      <div className='mb-4 flex flex-row'>
-        {/* <svg
+        <div className='mb-4 flex flex-row'>
+          {/* <svg
           xmlns='http://www.w3.org/2000/svg'
           enableBackground='new 0 0 24 24'
           height='36px'
@@ -59,13 +62,13 @@ const Requisites: React.FunctionComponent = () => {
             <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
           </g>
         </svg> */}
-        <div className='text-left '>
-          <div className=''>ОГРН: &nbsp;</div>
-          <div className=' '>1207800126247</div>
+          <div className='text-left '>
+            <div className=''>ОГРН: &nbsp;</div>
+            <div className=' '>1207800126247</div>
+          </div>
         </div>
-      </div>
-      <div className='mb-4 flex flex-row'>
-        {/* <svg
+        <div className='mb-4 flex flex-row'>
+          {/* <svg
           xmlns='http://www.w3.org/2000/svg'
           enableBackground='new 0 0 24 24'
           height='36px'
@@ -79,14 +82,14 @@ const Requisites: React.FunctionComponent = () => {
             <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
           </g>
         </svg> */}
-        <div className='text-left'>
-          <div className=''>Расчетный счет: &nbsp;</div>
-          <div className=' '>40702810403000066173</div>
+          <div className='text-left'>
+            <div className=''>Расчетный счет: &nbsp;</div>
+            <div className=' '>40702810403000066173</div>
+          </div>
         </div>
-      </div>
 
-      <div className='mb-4 flex flex-row'>
-        {/* <svg
+        <div className='mb-4 flex flex-row'>
+          {/* <svg
           xmlns='http://www.w3.org/2000/svg'
           enableBackground='new 0 0 24 24'
           height='36px'
@@ -100,13 +103,13 @@ const Requisites: React.FunctionComponent = () => {
             <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
           </g>
         </svg> */}
-        <div className='text-left'>
-          <div className=''>Корреспондентский счет: &nbsp;</div>
-          <div className=' '>30101810100000000723</div>
+          <div className='text-left'>
+            <div className=''>Корреспондентский счет: &nbsp;</div>
+            <div className=' '>30101810100000000723</div>
+          </div>
         </div>
-      </div>
-      <div className='mb-4 flex flex-row'>
-        {/* <svg
+        <div className='mb-4 flex flex-row'>
+          {/* <svg
           xmlns='http://www.w3.org/2000/svg'
           enableBackground='new 0 0 24 24'
           height='36px'
@@ -120,9 +123,19 @@ const Requisites: React.FunctionComponent = () => {
             <path d='M20.41,8.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 C21,9.3,20.79,8.79,20.41,8.41z M7,7h7v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10V13z' />
           </g>
         </svg> */}
-        <div className='text-left'>
-          <div className=''>ИНН/КПП: &nbsp;</div>
-          <div className=''>7801688448/783801001</div>
+          <div className='text-left'>
+            <div className=''>ИНН/КПП: &nbsp;</div>
+            <div className=''>7801688448/783801001</div>
+          </div>
+        </div>
+        <div>
+          <ArrowLink
+            className='mt-2'
+            download='Карточка предприятия.docx'
+            href='requisites/requisites.docx'
+          >
+            Скачать реквизиты
+          </ArrowLink>
         </div>
       </div>
     </div>
