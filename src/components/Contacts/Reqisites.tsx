28bcdb15db46917ada110ad8a849aca5e5fbbcb1
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2022-11-19 11:30:51 +0300

    change elements layout

--- src/components/Contacts/Reqisites.tsx
+++ src/components/Contacts/Reqisites.tsx
@@ -5,27 +5,27 @@ const Requisites: React.FunctionComponent = () => {
     <div className='mb-4  flex  flex-col items-start rounded border border-primary-600 px-4 text-xl shadow-xl'>
       <div>
         <h6 className=' my-4 font-semibold uppercase'>Реквизиты</h6>
-        <div className='mb-4 flex-col text-left'>
+        <div className='mb-4 flex flex-col text-left'>
           <div>Директор:</div>
           <div>Огурцов Андрей Сергеевич</div>
           <div>Действует на основании Устава</div>
         </div>
-        <div className='mb-4  flex-col text-left'>
+        <div className='mb-4 flex flex-col text-left'>
           <div>Полное Наименование:</div>
           <div>Общество с ограниченной ответственностью «ТехноПрогресс»</div>
         </div>
-        <div className='mb-4 flex-col text-left'>
+        <div className='mb-4 flex flex-col text-left'>
           <div>Сокращенное наименование:</div>
           <div>ООО «ТехноПрогресс»</div>
         </div>
-        <div className='mb-4 flex-col text-left'>
+        <div className='mb-4  flex flex-col text-left'>
           <div>Юридический адрес:</div>
           <div>
             190005, г. Санкт-Петербург, ул. 6-я Красноармейская, д. 10, лит. А,
             ПОМЕЩ. 24
           </div>
         </div>
-        <div className='mb-4 flex-col text-left'>
+        <div className='mb-4 flex flex-col text-left'>
           <div>Почтовый адрес:</div>
           <div>
             190005, г. Санкт-Петербург, ул. 6-я Красноармейская, д. 10, лит. А,
@@ -33,7 +33,7 @@ const Requisites: React.FunctionComponent = () => {
           </div>
         </div>
 
-        <div className='mb-4 flex-col text-left'>
+        <div className='mb-4 flex flex-col text-left'>
           <div></div>
           <div></div>
         </div>
